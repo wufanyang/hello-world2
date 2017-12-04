4ee2681fe85e0c96f92f1354c1a11b5deb806933
wufanyang <33921582+wufanyang@users.noreply.github.com>
2017-12-04 17:07:45 +0800

    Update catch_sigterm.py

--- catch_sigterm.py
+++ catch_sigterm.py
@@ -15,6 +15,11 @@ logger.setLevel(logging.INFO)
 def signal_term_handler(signal, frame):
     print 'got SIGTERM'
     logger.info('got SIGTERM')
+    print 'before sleep'
+    logger.info('before sleep')
+    time.sleep(60)
+    print 'after sleep'
+    logger.info('after sleep')
     sys.exit(0)
  
 signal.signal(signal.SIGTERM, signal_term_handler)
@@ -23,4 +28,3 @@ while True:
     time.sleep(3)
     print datetime.datetime.now()
     logger.info(datetime.datetime.now())
-
