3320657b614f02f061d580dc8e048ec918700322
wufanyang <33921582+wufanyang@users.noreply.github.com>
2017-12-04 15:37:15 +0800

    Update catch_sigterm.py

--- catch_sigterm.py
+++ catch_sigterm.py
@@ -1,8 +1,20 @@
 import signal
 import sys,time,datetime
- 
+import logging
+
+
+log_path = '/var/log/catch_sigterm.log'
+logger = logging.getLogger(__name__)
+hdlr = logging.FileHandler(log_path)
+formatter = logging.Formatter(
+    '%(pathname)s %(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
 def signal_term_handler(signal, frame):
     print 'got SIGTERM'
+    logger.info('got SIGTERM')
     sys.exit(0)
  
 signal.signal(signal.SIGTERM, signal_term_handler)
@@ -10,3 +22,5 @@ signal.signal(signal.SIGTERM, signal_term_handler)
 while True:
     time.sleep(3)
     print datetime.datetime.now()
+    logger.info(datetime.datetime.now())
+
